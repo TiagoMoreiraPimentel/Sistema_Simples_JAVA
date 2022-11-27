400144e445e287ab4e34ac6cf8606335d6d0849b
Tiago Pimentel <tiagomoreirapimentel@hotmail.com>
2022-11-27 17:59:40 -0300

    Logica de validação do login e senha implementado no botão 'Entrar'

--- Sistema_Simples_JAVA/src/main/java/View/ViewFormLogin.java
+++ Sistema_Simples_JAVA/src/main/java/View/ViewFormLogin.java
@@ -4,6 +4,8 @@
  */
 package View;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Tiago
@@ -100,7 +102,15 @@ public class ViewFormLogin extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        
+        if(txtLogin.getText().equals("tiago")&&txtSenha.getText().equals("123456")){
+            JOptionPane.showMessageDialog(null, "Bem vindo!");
+        }
+        else{
+            
+            JOptionPane.showMessageDialog(null, "Acesso negado!");
+        }
+        
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
